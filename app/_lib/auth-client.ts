342df7b2c595a737631071d4437791938a25bfa7
FabianoLaureano <fabianoandrelaureano@gmail.com>
2026-03-10 04:55:28 -0300

    rewrite

--- app/_lib/auth-client.ts
+++ app/_lib/auth-client.ts
@@ -1,7 +1,10 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3333",
+  baseURL:
+    typeof window !== "undefined"
+      ? window.location.origin
+      : process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
   fetchOptions: {
     credentials: "include",
   },
